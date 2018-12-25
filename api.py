db098f8297ac5c94d101b674e2d53b6ef3917734
SystemLLH <liuluhao0829@gmail.com>
2018-12-25 20:56:54 +0800

    add speed api(not complete)
    by tornado

---
+++ api.py
@@ -0,0 +1,8 @@
+from tornado.web import RequestHandler
+
+
+class PerMinuteSpeedHandler(RequestHandler):
+
+    def get(self, *args, **kwargs):
+        pass
+
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from scrapy import cmdline
 from data import init_database, open_database
+from tornado.web import Application
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from api import PerMinuteSpeedHandler
+
+
+def make_apiapp():
+    return Application([
+        (r"/speedperminute", PerMinuteSpeedHandler)
+    ])
 
 
 if __name__ == '__main__':
+    app = make_apiapp()
+    server = HTTPServer(app)
+    server.listen(8080)
     open_database()
     init_database()
     name = 'UserCrawler'
     cmdline.execute(('scrapy crawl ' + name).split())
+    try:
+        IOLoop.current().start()
+    except KeyboardInterrupt:
+        pass
